cb9a46662024934d6d8ebe651fb81dfa866cd71e
Alex Anderson <alexanderson1993@gmail.com>
2020-11-25 09:14:39 -0500

    Working to fix the errors in the tests.

--- test/jest.common.cjs
+++ test/jest.common.cjs
@@ -4,6 +4,8 @@ module.exports = {
   rootDir: path.join(__dirname, ".."),
   moduleDirectories: ["node_modules", "./test"],
   moduleNameMapper: {
+    "^server/(.*)$": "<rootDir>/server/$1",
+    "^client/(.*)$": "<rootDir>/client/$1",
     "\\.css$": require.resolve("./styleMocker.js"),
     "^url:": require.resolve("./styleMocker.js"),
     "^three/examples/jsm/(controls|loaders|objects|lines)/(DeviceOrientationControls|TrackballControls|OrbitControls|GLTFLoader|TransformControls|FlyControls|Sky|Reflector|LineGeometry|LineMaterial|Line2|DRACOLoader)": require.resolve(
