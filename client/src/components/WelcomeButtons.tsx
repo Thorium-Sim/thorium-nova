8b2be733b24578206214422c34ed794eb9e958b6
Alex Anderson <alexanderson1993@gmail.com>
2021-10-08 07:43:30 -0400

    Clean up the main App.tsx file a bit.

---
+++ client/src/components/WelcomeButtons.tsx
@@ -0,0 +1,63 @@
+import {NavLink} from "react-router-dom";
+import {useClientData} from "../context/useCardData";
+import Button from "@thorium/ui/Button";
+import {Disclosure} from "@headlessui/react";
+
+export const WelcomeButtons = ({className}: {className?: string}) => {
+  const client = useClientData();
+
+  return (
+    <div
+      className={`${className} flex flex-col justify-end self-end space-y-4 max-w-md h-full`}
+    >
+      {client.flight ? (
+        <NavLink className="btn btn-primary btn-outline" to="/flight">
+          Go To Flight Lobby
+        </NavLink>
+      ) : (
+        <>
+          <NavLink
+            className="btn btn-primary btn-outline"
+            to="/config/flight/quick"
+          >
+            Quick Start
+          </NavLink>
+          <NavLink
+            className="btn btn-secondary btn-outline"
+            to="/config/flight"
+          >
+            Custom Flight
+          </NavLink>
+          <Disclosure>
+            <Disclosure.Button className="btn btn-info btn-outline">
+              Load a Saved Flight
+            </Disclosure.Button>
+            <Disclosure.Panel
+              className="text-white list-none max-h-full overflow-y-auto"
+              as="ul"
+            >
+              {client.flights.length ? (
+                client.flights.map(f => (
+                  <li className="list-group-item" key={f.id}>
+                    <strong>{f.name}</strong>
+                    <br />
+                    <small>{new Date(f.date).toLocaleDateString()}</small>
+                  </li>
+                ))
+              ) : (
+                <>
+                  <li className="list-group-item">No Saved Flights</li>
+                </>
+              )}
+            </Disclosure.Panel>
+          </Disclosure>
+
+          <Button className="btn btn-warning btn-outline">Join a Server</Button>
+          <NavLink className="btn btn-alert btn-outline" to="/config">
+            Configure Plugins
+          </NavLink>
+        </>
+      )}
+    </div>
+  );
+};
---
+++ client/src/components/WelcomeLogo.tsx
@@ -0,0 +1,25 @@
+import {Link} from "react-router-dom";
+import Logo from "../images/logo.svg?url";
+import packageJson from "../../package.json";
+
+export const WelcomeLogo = ({className}: {className?: string}) => {
+  return (
+    <div className={className}>
+      <div className="flex items-end self-start ">
+        <img
+          draggable={false}
+          src={Logo}
+          alt="Thorium Logo"
+          className="max-h-32"
+        />
+        <h1 className="text-4xl ml-3 min-w-[12ch] text-white">Thorium Nova</h1>
+      </div>
+      <h2 className="text-2xl mt-2">
+        <Link className="text-purple-300 hover:text-purple-500" to="/releases">
+          Version {packageJson.version}
+        </Link>
+      </h2>
+      {/* <ClientButton /> */}
+    </div>
+  );
+};
