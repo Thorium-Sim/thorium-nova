19cc490ab7d84c0662e931f7b9dbb3d8db4cdbc3
Alex Anderson <alexanderson1993@gmail.com>
2021-10-18 09:49:05 -0400

    Add a timer system as an example system.

--- client/src/cards/dataList.ts
+++ client/src/cards/dataList.ts
@@ -1,6 +1,7 @@
 // The exported name _must_ be the same as the name of the card.
 export * as Clients from "./clients/data";
 export * as Flights from "./flights/data";
+export * as Timer from "./timer/data";
 
 // This is a special set of data that is available
 // on all cards.
---
+++ client/src/cards/timer/data.ts
@@ -0,0 +1,9 @@
+import {DataContext} from "server/src/utils/DataContext";
+
+export const subscriptions = {
+  timer: {
+    fetch: (context: DataContext) => {
+      // TODO: fetch timer data
+    },
+  },
+};
