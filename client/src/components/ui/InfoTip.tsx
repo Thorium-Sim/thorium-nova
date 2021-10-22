2b0ddd69661b4e4a5772827e5e4b6df52fbfc571
Alex Anderson <alexanderson1993@gmail.com>
2021-10-21 20:26:17 -0400

    Improvements to the styling of some of the UI components

--- client/src/components/ui/InfoTip.tsx
+++ client/src/components/ui/InfoTip.tsx
@@ -1,15 +1,12 @@
 import {Popover, Transition} from "@headlessui/react";
 import {ReactNode} from "react";
 import {FaInfoCircle} from "react-icons/fa";
-import Button from "./Button";
 
 const InfoTip = ({children}: {children: ReactNode}) => {
   return (
     <Popover className="relative">
-      <Popover.Button>
-        <Button className="btn btn-ghost btn-xs p-0">
-          <FaInfoCircle className="inline-block text-primary text-base cursor-pointer" />
-        </Button>
+      <Popover.Button className="btn btn-ghost btn-xs p-0">
+        <FaInfoCircle className="inline-block text-primary text-base cursor-pointer" />
       </Popover.Button>
       <Transition
         className="origin-center"
@@ -21,7 +18,7 @@ const InfoTip = ({children}: {children: ReactNode}) => {
         leaveTo="transform scale-95 opacity-0"
       >
         <Popover.Panel
-          className={`absolute bottom-8 max-w-xs z-10 border-transparent shadow-lg bg-opacity-90 bg-black rounded-lg p-2`}
+          className={`absolute bottom-8 transform -translate-x-1/2 max-w-xs w-max z-10 border-transparent shadow-lg bg-opacity-90 bg-black rounded-lg p-2`}
         >
           {children}
         </Popover.Panel>
