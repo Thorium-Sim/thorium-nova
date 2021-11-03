4b20d9be17d5494ae56154882d9de3db314b3e4c
Alex Anderson <alexanderson1993@gmail.com>
2021-11-03 18:52:18 -0400

    Rename NetSend, since it's not really a hook. This makes it even easier to use.

---
+++ client/src/context/netSend.tsx
@@ -0,0 +1,38 @@
+import {AllInputNames, AllInputParams, AllInputReturns} from "@thorium/inputs";
+import {getTabId} from "@thorium/tab-id";
+
+export async function netSend<
+  InputName extends AllInputNames,
+  Params extends AllInputParams[InputName],
+  Return extends AllInputReturns[InputName]
+>(type: InputName, params?: Params): Promise<Return> {
+  const clientId = await getTabId();
+  const body = new FormData();
+  body.append("input", type.toString());
+  let count = 0;
+  for (const key in params) {
+    const value = params[key];
+    if (value instanceof File) {
+      body.append(key, value);
+      params[key] = {} as any;
+    }
+    if (value instanceof Blob) {
+      body.append(key, value, `blob-${count++}`);
+      params[key] = {} as any;
+    }
+    if (value instanceof FileList) {
+      for (let i = 0; i < value.length; i++) {
+        body.append(`${key}[]`, value[i]);
+      }
+      params[key] = [] as any;
+    }
+  }
+  body.append("params", JSON.stringify(params));
+  const response = await fetch(`/netSend`, {
+    method: "POST",
+    headers: {authorization: `Bearer ${clientId}`},
+    body,
+  });
+  const json = await response.json();
+  return json;
+}
