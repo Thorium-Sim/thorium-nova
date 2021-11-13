d7bff648a6d56a99c5553548e8e7e3332aaffaf8
Alex Anderson <alexanderson1993@gmail.com>
2021-11-13 10:05:11 -0500

    Only fetch stations for active plugins

--- server/src/netRequests/plugins/stations.ts
+++ server/src/netRequests/plugins/stations.ts
@@ -5,6 +5,7 @@ export const pluginStationsRequest = {
   availableStationsList(dataContext: DataContext) {
     return dataContext.server.plugins
       .reduce((stations: StationComplementPlugin[], plugin) => {
+        if (!plugin.active) return stations;
         return stations.concat(plugin.aspects.stationComplements);
       }, [])
       .map(station => ({
