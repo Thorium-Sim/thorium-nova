c8e6156aabad81b7e06d2c366b6d95a1dd8ecbd1
Alex Anderson <alexanderson1993@users.noreply.github.com>
2021-11-20 06:59:38 -0500

    Add the API for getting the ships available for a flight. (#112)

--- server/src/netRequests/plugins/ships.ts
+++ server/src/netRequests/plugins/ships.ts
@@ -1,3 +1,4 @@
+import ShipPlugin from "server/src/classes/Plugins/Ship";
 import {DataContext} from "server/src/utils/DataContext";
 import {getPlugin} from "./utils";
 
@@ -21,5 +22,20 @@ export const pluginShipsRequest = {
     const ship = plugin.aspects.ships.find(ship => ship.name === params.shipId);
     if (!ship) throw null;
     return ship;
+  },
+  availableShips(context: DataContext) {
+    return context.server.plugins
+      .reduce((ships: ShipPlugin[], plugin) => {
+        if (!plugin.active) return ships;
+        // TODO November 13, 2021 - Filter out ships that don't have the necessary
+        // components for being a player ship.
+        return ships.concat(plugin.aspects.ships);
+      }, [])
+      .map(ship => ({
+        name: ship.name,
+        description: ship.description,
+        vanityUrl: ship.toJSON().assets.vanity,
+        pluginName: ship.pluginName,
+      }));
   },
 };
