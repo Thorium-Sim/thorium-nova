2829e0c341da4a72fe6f2473fc959ebb138f78fb
Alex Anderson <alexanderson1993@gmail.com>
2021-12-04 11:33:21 -0500

    feat(Themes): Add the ability to assign a theme to a ship. Closes #133

--- client/src/pages/Config/Ships/Basic.tsx
+++ client/src/pages/Config/Ships/Basic.tsx
@@ -1,14 +1,18 @@
 import {Navigate, useParams, useNavigate} from "react-router-dom";
 import {useNetRequest} from "client/src/context/useNetRequest";
-import {useState} from "react";
+import {Fragment, useState} from "react";
 import Input from "@thorium/ui/Input";
 import TagInput from "@thorium/ui/TagInput";
 import {netSend} from "client/src/context/netSend";
 import {toast} from "client/src/context/ToastContext";
+import {Popover, Transition} from "@headlessui/react";
+import {FaChevronDown} from "react-icons/fa";
+import SearchableList from "@thorium/ui/SearchableList";
 
 export function Basic() {
   const {pluginId, shipId} = useParams() as {pluginId: string; shipId: string};
   const data = useNetRequest("pluginShips", {pluginId});
+  const allThemes = useNetRequest("pluginAllThemes");
   const ship = data.find(d => d.name === shipId);
   const [error, setError] = useState(false);
   const navigate = useNavigate();
@@ -46,37 +50,35 @@ export function Basic() {
             />
           </div>
           <div className="pb-4">
-            <label className="w-full">
-              <Input
-                labelHidden={false}
-                label="Description"
-                defaultValue={ship.description}
-                onBlur={(e: any) =>
-                  netSend("pluginShipUpdate", {
-                    pluginId,
-                    shipId,
-                    description: e.target.value,
-                  })
-                }
-              />
-            </label>
+            <Input
+              as="textarea"
+              className="!h-32"
+              labelHidden={false}
+              label="Description"
+              defaultValue={ship.description}
+              onBlur={(e: any) =>
+                netSend("pluginShipUpdate", {
+                  pluginId,
+                  shipId,
+                  description: e.target.value,
+                })
+              }
+            />
           </div>
           <div className="pb-4">
-            <label className="w-full">
-              <Input
-                labelHidden={false}
-                label="Category"
-                type="textarea"
-                defaultValue={ship.category}
-                onBlur={(e: any) =>
-                  netSend("pluginShipUpdate", {
-                    pluginId,
-                    shipId,
-                    category: e.target.value,
-                  })
-                }
-              />
-            </label>
+            <Input
+              labelHidden={false}
+              label="Category"
+              type="textarea"
+              defaultValue={ship.category}
+              onBlur={(e: any) =>
+                netSend("pluginShipUpdate", {
+                  pluginId,
+                  shipId,
+                  category: e.target.value,
+                })
+              }
+            />
           </div>
           <TagInput
             label="Tags"
@@ -98,6 +100,68 @@ export function Basic() {
               });
             }}
           />
+          <label>Theme</label>
+          <div className="flex flex-wrap items-center">
+            <p>
+              {ship.theme?.themeId ? (
+                <>
+                  {ship.theme.themeId} ({ship.theme.pluginId})
+                </>
+              ) : (
+                "Default Theme"
+              )}
+            </p>
+            <Popover className="relative">
+              {({open, close}) => (
+                <>
+                  <Popover.Button
+                    className={`btn btn-primary ml-4
+             ${open ? "" : "text-opacity-90"}
+             group`}
+                  >
+                    <span>Change Theme</span>
+                    <FaChevronDown
+                      className={`${open ? "" : "text-opacity-70"}
+               ml-2 h-5 w-5 group-hover:text-opacity-80 transition ease-in-out duration-150`}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute z-10 w-screen max-w-sm mt-3 sm:px-0">
+                      <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5  backdrop-filter backdrop-blur bg-black/70 p-4">
+                        <SearchableList
+                          items={
+                            allThemes.map(t => ({
+                              id: t,
+                              label: t.themeId,
+                              category: t.pluginId,
+                            })) || []
+                          }
+                          selectedItem={ship.theme}
+                          setSelectedItem={item => {
+                            netSend("pluginShipUpdate", {
+                              pluginId,
+                              shipId,
+                              theme: item,
+                            });
+                            close();
+                          }}
+                        ></SearchableList>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+          </div>
         </div>
       </div>
     </fieldset>
