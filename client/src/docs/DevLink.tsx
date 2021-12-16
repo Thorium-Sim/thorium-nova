cc3427a333e9ed40a5a438c7ded96b6a8a4c852c
Alex Anderson <alexanderson1993@gmail.com>
2021-12-16 09:56:30 -0500

    More cleanup

--- client/src/docs/DevLink.tsx
+++ client/src/docs/DevLink.tsx
@@ -1,17 +1,6 @@
 export default function DevLink({children}: {children: string}) {
   return (
-    <button
-      className="text-purple-300 font-mono"
-      onClick={() => {
-        if (process.env.NODE_ENV === "development") {
-          fetch(
-            `/__open-in-editor?file=${encodeURIComponent(
-              `${process.env.USER_PATH}${children}`
-            )}`
-          );
-        }
-      }}
-    >
+    <button className="text-purple-300 font-mono" onClick={() => {}}>
       {children}
     </button>
   );
