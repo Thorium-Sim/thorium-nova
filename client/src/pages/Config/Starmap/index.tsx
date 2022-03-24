ee662d52789d4a505fdf55dad4b4fdfd5ebf56cb
Alex Anderson <alexanderson1993@gmail.com>
2022-03-24 19:23:26 -0400

    Working on the styling for the editor palette.

--- client/src/pages/Config/Starmap/index.tsx
+++ client/src/pages/Config/Starmap/index.tsx
@@ -26,7 +26,13 @@ import {
 import {netSend} from "client/src/context/netSend";
 import {toast} from "client/src/context/ToastContext";
 import {useConfirm} from "@thorium/ui/AlertDialog";
-
+import {Portal} from "@headlessui/react";
+import {useDrag} from "@use-gesture/react";
+import {animated, config, useSpring} from "@react-spring/web";
+import {IoMdClose} from "react-icons/io";
+import {FaMinus, FaTimes} from "react-icons/fa";
+import Input from "@thorium/ui/Input";
+import {useLocalStorage} from "client/src/hooks/useLocalStorage";
 const FAR = 1e27;
 
 interface SceneRef {
@@ -66,6 +72,68 @@ function useSynchronizeSystemId() {
 
   return storedSystemId;
 }
+
+function EditorPalette() {
+  const [[x, y], setXY] = useLocalStorage(
+    "starmapEditorPalettePosition",
+    [0, 0]
+  );
+
+  const bind = useDrag(
+    ({offset: [x, y]}) => {
+      setXY([x, y]);
+    },
+    {
+      from: [x, y],
+      filterTaps: true,
+    }
+  );
+  const [minimized, setMinimized] = useState(false);
+  const selectedItem = useStarmapStore(store => store.selectedObjectId);
+
+  if (!selectedItem) return null;
+
+  return (
+    <Portal>
+      <animated.div
+        className="w-64 max-h-96 bg-gray-900 shadow-lg rounded-lg fixed left-[calc(50%-6rem)] top-[calc(50%-8rem)]"
+        style={{
+          x,
+          y,
+        }}
+      >
+        <div
+          className={`w-full h-8 bg-gray-800 text-white font-bold flex items-center justify-between select-none touch-none cursor-grab active:cursor-grabbing rounded-t-lg ${
+            minimized ? "rounded-b-lg" : ""
+          }`}
+          {...bind()}
+        >
+          <button
+            className="p-1 ml-1 rounded-full hover:bg-white/10 cursor-pointer"
+            onClick={() => useStarmapStore.setState({selectedObjectId: null})}
+            aria-label="Close"
+          >
+            <FaTimes />
+          </button>
+          <span className="flex-1 text-center">Editor</span>
+          <button
+            className="p-1 mr-1 rounded-full hover:bg-white/10 cursor-pointer"
+            onClick={() => setMinimized(s => !s)}
+            aria-label="Minimize"
+          >
+            <FaMinus />
+          </button>
+        </div>
+        {minimized ? null : (
+          <div className="w-full h-full overflow-y-auto p-2 text-white">
+            <Input label="Name" />
+          </div>
+        )}
+      </animated.div>
+    </Portal>
+  );
+}
+
 export default function StarMap() {
   const {pluginId} = useParams() as {
     pluginId: string;
@@ -155,6 +223,7 @@ export default function StarMap() {
           Edit
         </Button>
       </Menubar>
+      <EditorPalette />
       <div className="h-[calc(100%-2rem)]  relative bg-black">
         <Canvas
           onContextMenu={e => {
