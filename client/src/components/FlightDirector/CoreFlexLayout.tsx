2cb91aecd8e4380a437efe9c2ef5bfbf398592ae
Alex Anderson <alexanderson1993@gmail.com>
2022-04-16 21:03:41 -0400

    feat(Flight Director): Add the ability to save flight director flex layouts to ThoriumSim accounts. Closes #278

--- client/src/components/FlightDirector/CoreFlexLayout.tsx
+++ client/src/components/FlightDirector/CoreFlexLayout.tsx
@@ -1,16 +1,12 @@
-import {IJsonModel, Layout, Model, TabNode} from "flexlayout-react";
-import {forwardRef, Suspense, useState} from "react";
+import {Layout, TabNode} from "flexlayout-react";
+import {forwardRef, Suspense, useContext} from "react";
 import * as Cores from "../../cores";
 import CardProvider from "client/src/context/CardContext";
 import {LoadingSpinner} from "@thorium/ui/LoadingSpinner";
-import {useLocalStorage} from "client/src/hooks/useLocalStorage";
+import {CoreFlexLayoutContext} from "./CoreFlexLayoutContext";
 
 export const CoreFlexLayout = forwardRef<Layout>((_, ref) => {
-  const [initialModel, setInitialModel] = useLocalStorage<IJsonModel>(
-    "core-flexLayout",
-    defaultJson
-  );
-  const [layoutModel] = useState(() => Model.fromJson(initialModel));
+  const {layoutModel, setInitialModel} = useContext(CoreFlexLayoutContext);
 
   return (
     <Layout
@@ -25,7 +21,9 @@ export const CoreFlexLayout = forwardRef<Layout>((_, ref) => {
     />
   );
 });
-function FlexCore({compName}: {compName: keyof typeof Cores}) {
+
+function flexLayoutFactory(node: TabNode) {
+  var compName = node.getComponent() as keyof typeof Cores;
   const Core = Cores[compName];
   if (Core)
     return (
@@ -37,43 +35,3 @@ function FlexCore({compName}: {compName: keyof typeof Cores}) {
     );
   return null;
 }
-function flexLayoutFactory(node: TabNode) {
-  var component = node.getComponent() as keyof typeof Cores;
-
-  return <FlexCore compName={component} />;
-}
-const defaultJson: IJsonModel = {
-  global: {
-    splitterSize: 1,
-    splitterExtra: 4,
-    tabEnableFloat: true,
-    tabSetEnableClose: true,
-    tabSetMinWidth: 100,
-    tabSetMinHeight: 100,
-    borderMinSize: 100,
-    borderEnableAutoHide: true,
-    enableEdgeDock: true,
-  },
-  borders: [
-    {
-      type: "border",
-      location: "bottom",
-      children: [],
-    },
-    {
-      type: "border",
-      location: "left",
-      children: [],
-    },
-    {
-      type: "border",
-      location: "right",
-      children: [],
-    },
-  ],
-  layout: {
-    type: "row",
-
-    children: [],
-  },
-};
