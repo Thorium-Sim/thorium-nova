ee0dd5092a0222df500db810ea39a8d8395911c3
Alex Anderson <alexanderson1993@gmail.com>
2022-05-14 10:07:23 -0400

    Add the necessary inputs for the warp engine system. Closes #312

---
+++ server/src/inputs/shipSystems/warpEngines.ts
@@ -0,0 +1,29 @@
+import {DataContext} from "server/src/utils/DataContext";
+import {pubsub} from "server/src/utils/pubsub";
+import {getShipSystem} from "./getShipSystem";
+
+export const warpEnginesInputs = {
+  warpEnginesSetWarpFactor: (
+    context: DataContext,
+    params: {systemId?: number; factor: number}
+  ) => {
+    const system = getShipSystem(context, {
+      systemId: params.systemId,
+      systemType: "warpEngines",
+    });
+    if (!system.components.isWarpEngines)
+      throw new Error("System is not a warp engines outfit");
+    const {interstellarCruisingSpeed, solarCruisingSpeed} =
+      system.components.isWarpEngines;
+
+    system.updateComponent("isWarpEngines", {
+      currentWarpFactor: params.factor,
+    });
+    // @ts-expect-error We can remove this once the warp engines card data is defined.
+    pubsub.publish("warpEngines", {
+      shipId: context.ship?.id,
+      systemId: system.id,
+    });
+    return system;
+  },
+};
