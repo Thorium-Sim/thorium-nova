245f263df0edfb3c45fc000a647e1a5d4e8c3955
Alex Anderson <alexanderson1993@gmail.com>
2022-06-10 07:55:37 -0400

    Clean up some logs.

--- client/src/utils/clientSocket.ts
+++ client/src/utils/clientSocket.ts
@@ -1,14 +1,10 @@
 import {SnapshotInterpolation, Types} from "@geckos.io/snapshot-interpolation";
-import {InterpolatedSnapshot} from "@geckos.io/snapshot-interpolation/lib/types";
 import {decode} from "@msgpack/msgpack";
 import EventEmitter from "eventemitter3";
 import ReconnectingWebSocket from "reconnecting-websocket";
 import {SERVER_FPS} from "server/src/utils/constants";
 
 export let SI = new SnapshotInterpolation(SERVER_FPS);
-
-let dataSize = 0;
-let time = Date.now();
 export class ClientSocket extends EventEmitter {
   constructor(private socket: ReconnectingWebSocket) {
     super();
@@ -17,20 +13,12 @@ export class ClientSocket extends EventEmitter {
         const reader = new FileReader();
         reader.addEventListener("loadend", () => {
           if (reader.result instanceof ArrayBuffer) {
-            if (Date.now() - time > 1000) {
-              // log in kilobits per second
-              console.log(`${(dataSize / 1024) * 8} kbps`);
-              time = Date.now();
-              dataSize = 0;
-            }
-            dataSize += reader.result.byteLength;
             const data = decode(reader.result) as
               | {type: string; data: any}
               | Types.Snapshot;
             if ("type" in data) {
               this.emit(data.type, data.data);
             } else {
-              console.log(data.state.length);
               SI.snapshot.add(data);
             }
           }
