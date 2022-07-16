45db3a14d6a5b60a5ab480ae644174d54a6640fd
Alex Anderson <alexanderson1993@gmail.com>
2022-07-16 13:09:07 -0400

    Various improvements.

---
+++ client/src/hooks/useSessionStorage.tsx
@@ -0,0 +1,37 @@
+import {useReducer, useEffect, useCallback, Reducer} from "react";
+
+export function useSessionStorageReducer<R extends Reducer<I, any>, I>(
+  reducer: R,
+  defaultState: I,
+  storageKey: string
+) {
+  const init = useCallback(() => {
+    let preloadedState;
+    try {
+      preloadedState = JSON.parse(
+        window.sessionStorage.getItem(storageKey) || ""
+      );
+      // validate preloadedState if necessary
+    } catch (e) {
+      // ignore
+    }
+    return preloadedState || defaultState;
+  }, [storageKey, defaultState]);
+
+  const hookVars = useReducer(reducer, null, init);
+
+  const hookyHook = hookVars[0];
+  useEffect(() => {
+    sessionStorage.setItem(storageKey, JSON.stringify(hookyHook));
+  }, [storageKey, hookyHook]);
+
+  return hookVars;
+}
+
+export function useSessionStorage<T>(storageKey: string, defaultValue: T) {
+  return useSessionStorageReducer(
+    (state: T, action: T) => action,
+    defaultValue,
+    storageKey
+  );
+}
