9a6b6b721f8b0b279a31712eb0edf0bf221a0ae6
Alex Anderson <alexanderson1993@gmail.com>
2022-08-19 20:04:52 -0400

    Fix for getShipSystem function.

--- server/src/inputs/shipSystems/getShipSystem.ts
+++ server/src/inputs/shipSystems/getShipSystem.ts
@@ -1,20 +1,24 @@
 import {DataContext} from "server/src/utils/DataContext";
 import {Entity} from "server/src/utils/ecs";
-
+import {pascalCase} from "change-case";
 export function getShipSystem(
   context: DataContext,
   param: {systemType: string} | {systemId: number}
 ) {
   let system: Entity | undefined | null;
-  if ("systemId" in param) {
+  if ("systemId" in param && param.systemId) {
     system = context.flight?.ecs.getEntityById(param.systemId);
-  } else {
+  } else if ("systemType" in param) {
     const ship = context.ship;
     system = ship?.components.shipSystems?.shipSystemIds.reduce(
       (sys: undefined | Entity, id) => {
         if (sys) return sys;
         const entity = context.flight?.ecs.getEntityById(id);
-        if (entity?.components.isShipSystem?.type === param.systemType) {
+
+        if (
+          entity?.components &&
+          `is${pascalCase(param.systemType)}` in entity.components
+        ) {
           return entity;
         }
       },
