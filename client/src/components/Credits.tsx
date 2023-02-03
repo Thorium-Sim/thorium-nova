9f2bafd1117892079b3a5fefeba0de7e2dd09c2e
Alex Anderson <alexanderson1993@gmail.com>
2023-02-02 19:23:44 -0500

    Add Tanner to the credits.

--- client/src/components/Credits.tsx
+++ client/src/components/Credits.tsx
@@ -1,22 +1,22 @@
 import styles from "./Credits.module.css";
 const creditList = [
   {
-    key: "alex",
     header: "Created By",
     content: "Alex Anderson",
   },
   {
-    key: "crystal",
     header: "Strategy & Design",
     content: "Crystal Anderson",
   },
   {
-    key: "victor",
+    header: "Development",
+    content: "Tanner Chamberlain",
+  },
+  {
     header: "Inspiration",
     content: "Victor Williamson",
   },
   {
-    key: "contributors-welcome",
     header: (
       <a
         href="https://github.com/Thorium-Sim/thorium-nova/blob/develop/CONTRIBUTING.md"
@@ -51,15 +51,15 @@ const Credits = ({className}: {className?: string}) => {
         className={`${styles["all-credits"]} my-8`}
         style={{["--credits-duration" as any]: `${creditList.length * 2}s`}}
       >
-        {creditList.map(c => (
-          <div key={c.key} className="text-white py-2">
+        {creditList.map((c, i) => (
+          <div key={i} className="text-white py-2">
             <p>{c.header}</p>
             <p className="font-bold text-lg mt-1">{c.content}</p>
           </div>
         ))}
         <div className="h-8"></div>
-        {creditList.map(c => (
-          <div key={c.key} className="text-white py-2">
+        {creditList.map((c, i) => (
+          <div key={i} className="text-white py-2">
             <p>{c.header}</p>
             <p className="font-bold text-lg mt-1">{c.content}</p>
           </div>
