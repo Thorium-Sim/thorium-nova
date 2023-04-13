aed2b609d04186269f0fd15b56137536352f846c
Alex Anderson <alexanderson1993@gmail.com>
2023-04-13 14:39:05 -0400

    Update QuoteOfTheDay.tsx

--- client/src/components/QuoteOfTheDay.tsx
+++ client/src/components/QuoteOfTheDay.tsx
@@ -258,7 +258,9 @@ const quotes = [
   "Leading others to a promised land that you yourself will never set foot in.",
   "Your heart knows the direction. Run!",
   // Natalie's Bishop's Wife. Also maybe Draconius
-  "I'm tired of being a wanderer. I'm tired of an existence where one is neither hot nor cold, hungry nor full."
+  "I'm tired of being a wanderer. I'm tired of an existence where one is neither hot nor cold, hungry nor full.",
+  // The Little Prince
+  "If you want to build a ship, don't drum up the people to gather wood, divide the work, and give orders. Instead, teach them to yearn for the vast and endless sea."
 ];
 
 const QuoteOfTheDay = () => {
