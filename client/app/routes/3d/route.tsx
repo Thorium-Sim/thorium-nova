66d025bfc9b6f21a5aa047c993d4bf42e71731e7
Alex Anderson <hey@ralexanderson.com>
2024-05-19 13:14:07 -0400

    Adding spin

--- client/app/routes/3d/route.tsx
+++ client/app/routes/3d/route.tsx
@@ -1,36 +1,24 @@
-import {
-	AsciiRenderer,
-	Edges,
-	OrbitControls,
-	Outlines,
-	Stars,
-} from "@react-three/drei";
-import {
-	Canvas,
-	type GroupProps,
-	useFrame,
-	useLoader,
-} from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+import { Canvas, type GroupProps, useFrame } from "@react-three/fiber";
 import {
 	type ReactNode,
-	useEffect,
-	useLayoutEffect,
 	useMemo,
 	useRef,
+	createContext,
+	useContext,
+	useState,
+	forwardRef,
+	useEffect,
 } from "react";
 import {
 	type InstancedMesh,
-	TextureLoader,
-	Vector4,
 	Matrix4,
 	Vector3,
 	Quaternion,
 	Color,
-	AdditiveBlending,
-	Blending,
+	Group,
+	MathUtils,
 } from "three";
-import blobImage from "./blob.png";
-import { randomPointOnSphere } from "@thorium/randomPoint/randomPointInSphere";
 
 export default function ThreeD() {
 	return (
@@ -38,19 +26,7 @@ export default function ThreeD() {
 			<Canvas>
 				<ambientLight intensity={0.1} />
 				<directionalLight color="white" position={[2, 3, 5]} />
-				<Emitter rotation={[0, Math.PI / 2, 0]}>
-					<Cell
-						initialCount={1}
-						lifeInSeconds={1}
-						lifeVariance={0.5}
-						birthRatePerSecond={2}
-						birthRateVariance={1}
-						speedVariance={0.9}
-					>
-						<boxGeometry attach="geometry" args={[0.5, 0.5, 0.5]} />
-						<meshStandardMaterial attach="material" color="yellow" />
-					</Cell>
-				</Emitter>
+				<Test />
 				<OrbitControls />
 				<Stars />
 			</Canvas>
@@ -58,41 +34,135 @@ export default function ThreeD() {
 	);
 }
 
+function Test() {
+	const ref = useRef<Group>(null);
+	useFrame((state) => {
+		if (!ref.current) return;
+		ref.current.position.y = Math.sin(state.clock.elapsedTime) * 2;
+	});
+	return (
+		<Emitter
+			ref={ref}
+			rotation={[0, Math.PI / 2, 0]}
+			emissionAngleRange={{ latitude: 0, longitude: 0 }}
+		>
+			<mesh>
+				<boxGeometry attach="geometry" args={[0.5, 0.5, 0.5]} />
+				<meshBasicMaterial wireframe attach="material" color="white" />
+			</mesh>
+			<Cell
+				initialCount={0}
+				lifeInSeconds={1}
+				lifeVariance={0.5}
+				birthRatePerSecond={20}
+				birthRateVariance={10}
+				speed={2}
+				speedVariance={0.9}
+				attachToEmitter
+				// angleVariance={{ longitude: 180, latitude: 90 }}
+				spin={{ longitude: 0, latitude: 1 }}
+				colorOverLife={
+					new LinearSpline({
+						points: [
+							[0, new Color("yellow")],
+							[0.9, new Color("red")],
+						],
+					})
+				}
+				scaleVariance={0.5}
+				scalePercentOverLife={
+					new LinearSpline({
+						points: [
+							[0, 0],
+							[0.2, 1],
+							[0.9, 1],
+							[1, 0],
+						],
+					})
+				}
+			>
+				<coneGeometry attach="geometry" args={[0.5, 0.75, 3, 1]} />
+				<meshBasicMaterial wireframe attach="material" color="white" />
+			</Cell>
+		</Emitter>
+	);
+}
+
+interface Angle {
+	longitude: number;
+	latitude: number;
+}
+const EmitterContext = createContext<{
+	emissionAngleRange: Angle;
+	getEmitterPosition: () => Vector3;
+}>({
+	emissionAngleRange: { longitude: 0, latitude: 0 },
+	getEmitterPosition: () => new Vector3(0, 0, 0),
+});
+
+const useEmitter = () => {
+	return useContext(EmitterContext);
+};
+
 /**
  * A component that emits children based on the specified angular range in 3D space.
  *
  * @param {object} props - The properties for the Emitter component.
  * @param {ReactNode} props.children - The children to be emitted.
  * @param {object} [props.emissionAngleRange] - How the emission angle varies in 3D space both positive and negative.
- * @param {number} [props.emissionAngleRange.azimuthal=0] - The azimuthal angle (longitude) from 0˚ to 180˚.
- * @param {number} [props.emissionAngleRange.polar=0] - The polar angle (latitude) from 0˚ to 90˚.
+ * @param {number} [props.emissionAngleRange.longitude=0] - The azimuthal angle from 0˚ to 180˚.
+ * @param {number} [props.emissionAngleRange.latitude=0] - The polar angle from 0˚ to 90˚.
  * @returns {ReactNode} The emitted children.
  */
-function Emitter({
-	children,
-	emissionAngleRange = { azimuthal: 0, polar: 0 },
-	...props
-}: {
-	children: ReactNode;
-	emissionAngleRange?: { azimuthal: number; polar: number };
-} & GroupProps) {
-	return <group {...props}>{children}</group>;
-}
+const Emitter = forwardRef<
+	Group,
+	{
+		children: ReactNode;
+		emissionAngleRange?: Angle;
+	} & GroupProps
+>(
+	(
+		{ children, emissionAngleRange = { longitude: 0, latitude: 0 }, ...props },
+		forwardedRef,
+	) => {
+		const innerRef = useRef<Group>(null);
+		const ref = mergeRefs([forwardedRef, innerRef]);
+		return (
+			<group {...props} ref={ref}>
+				<EmitterContext.Provider
+					value={{
+						emissionAngleRange,
+						getEmitterPosition: () => innerRef.current?.position!,
+					}}
+				>
+					{children}
+				</EmitterContext.Provider>
+			</group>
+		);
+	},
+);
 
-let count = 0;
+Emitter.displayName = "Emitter";
+
 const instanceMatrix = new Matrix4();
 const position = new Vector3();
 const quaternion = new Quaternion();
-const scale = new Vector3();
-const color = new Color();
+const meshScale = new Vector3();
+const meshColor = new Color();
+const velocity = new Vector3();
 const forward = new Vector3(0, 0, 1);
+const up = new Vector3(0, 1, 0);
 interface InstanceProps {
 	/** How long the particle has been alive */
-	lifetime: number;
+	age: number;
 	/** How long the particle is supposed to live for */
 	lifespan: number;
-	/** How fast the particle goes */
-	speed: number;
+	/** How fast the particle goes in each direction */
+	velocity: [number, number, number];
+	/** How big the particle is */
+	scale: number;
+	/** The position of the emitter when the particle was created */
+	position: [number, number, number];
 }
 function Cell({
 	children,
@@ -103,6 +173,14 @@ function Cell({
 	lifeVariance = 0,
 	speed = 1,
 	speedVariance = 0,
+	colorOverLife,
+	scale = 1,
+	scaleVariance = 0,
+	scalePercentOverLife,
+	angle = {},
+	angleVariance = {},
+	attachToEmitter = false,
+	rng = Math.random,
 }: {
 	children: ReactNode;
 	birthRatePerSecond?: number;
@@ -112,22 +190,23 @@ function Cell({
 	lifeVariance?: number;
 	speed?: number;
 	speedVariance?: number;
-
-	// TODO
-	angle?: unknown;
-	angleVariance?: unknown;
-	spin?: unknown;
-	spinVariance?: unknown;
-	alignAngleWithVelocity?: boolean;
 	colorOverLife?: LinearSpline<Color>;
-	opacityOverLife?: LinearSpline<number>;
 	scale?: number;
 	scaleVariance?: number;
-	scaleOverLife?: LinearSpline<number>;
+	scalePercentOverLife?: LinearSpline<number>;
+	angle?: Partial<Angle>;
+	angleVariance?: Partial<Angle>;
+	spin?: Partial<Angle>;
+	spinVariance?: Partial<Angle>;
+
 	attachToEmitter?: boolean;
-	randomSeed?: number;
+	// TODO
+	alignAngleWithVelocity?: boolean;
+
+	rng?: () => number;
 }) {
 	const ref = useRef<InstancedMesh>(null);
+	const { emissionAngleRange, getEmitterPosition } = useEmitter();
 
 	const maxParticles = useMemo(
 		() =>
@@ -149,18 +228,19 @@ function Cell({
 
 	useFrame((state, delta) => {
 		if (!ref.current) return;
+		const emitterPosition = getEmitterPosition();
+
 		const { current: mesh } = ref;
 		if (!mesh.userData.instanceProps) {
 			mesh.userData.instanceProps = new Map();
 			mesh.userData.activeParticles = [];
 			mesh.userData.inactiveParticles = [];
 			mesh.userData.timeToNextParticle =
-				birthRatePerSecond + (Math.random() - 0.5) * 2 * birthRateVariance;
-			let i = 0;
-			for (i = 0; i < initialCount; i++) {
+				1 / (birthRatePerSecond + (rng() - 0.5) * 2 * birthRateVariance);
+			for (let i = 0; i < initialCount; i++) {
 				mesh.userData.activeParticles.push(i);
 			}
-			for (; i < maxParticles; i++) {
+			for (let i = initialCount; i < maxParticles; i++) {
 				mesh.userData.inactiveParticles.push(i);
 			}
 		}
@@ -172,62 +252,105 @@ function Cell({
 				const particleIndex = mesh.userData.inactiveParticles.pop();
 				mesh.userData.activeParticles.push(particleIndex);
 			}
-			const rate =
-				birthRatePerSecond + (Math.random() - 0.5) * 2 * birthRateVariance;
+			const rate = birthRatePerSecond + (rng() - 0.5) * 2 * birthRateVariance;
 			mesh.userData.timeToNextParticle = 1 / rate;
 			// We might need to emit two or more particles in a single frame. This makes sure that happens.
 			mesh.userData.timeToNextParticle -= difference;
 		}
+
 		for (let i = 0; i < maxParticles; i++) {
 			mesh.getMatrixAt(i, instanceMatrix);
-			instanceMatrix.decompose(position, quaternion, scale);
+			instanceMatrix.decompose(position, quaternion, meshScale);
+
 			// If the particle is dead, skip it
 			if (!mesh.userData.activeParticles.includes(i)) {
-				scale.setScalar(0);
-				instanceMatrix.compose(position, quaternion, scale);
+				meshScale.setScalar(0);
+				instanceMatrix.compose(position, quaternion, meshScale);
 				mesh.setMatrixAt(i, instanceMatrix);
 				continue;
 			}
 
 			let instanceProps: InstanceProps = mesh.userData.instanceProps.get(i);
-			count++;
-			if (count > 30) {
-				count = 0;
-			}
 
 			// Initialize the particle
 			if (!instanceProps) {
 				position.set(0, 0, 0);
 				quaternion.set(0, 0, 0, 1);
-				scale.set(1, 1, 1);
-				color.set(1, 1, 1);
+				meshScale.set(1, 1, 1);
+				meshColor.set(1, 1, 1);
+				const speedValue = speed + (rng() - 0.5) * 2 * speedVariance;
 
+				// Randomize the direction of the particle
+				{
+					const phi =
+						(rng() - 0.5) *
+						2 *
+						MathUtils.degToRad(emissionAngleRange.longitude);
+					const theta =
+						(rng() - 0.5) * 2 * MathUtils.degToRad(emissionAngleRange.latitude);
+					const x = Math.cos(theta) * Math.sin(phi);
+					const y = Math.sin(theta);
+					const z = Math.cos(theta) * Math.cos(phi);
+					velocity.set(x, y, z).normalize().multiplyScalar(speedValue);
+				}
+				// Randomize the rotation of the particle
+				{
+					const latitude =
+						(angle.latitude || 0) +
+						(rng() - 0.5) * 2 * (angleVariance.latitude || 0);
+					const longitude =
+						(angle.longitude || 0) +
+						(rng() - 0.5) * 2 * (angleVariance.longitude || 0);
+					const x = Math.cos(latitude) * Math.cos(longitude);
+					const y = Math.cos(latitude) * Math.sin(longitude);
+					const z = Math.sin(latitude);
+					// Reusing this vector to avoid creating a new one
+					forward.set(x, y, z);
+					quaternion.setFromUnitVectors(up, forward);
+				}
 				instanceProps = {
-					lifetime: 0,
-					lifespan: lifeInSeconds + (Math.random() - 0.5) * 2 * lifeVariance,
-					speed: speed + (Math.random() - 0.5) * 2 * speedVariance,
+					age: 0,
+					lifespan: lifeInSeconds + (rng() - 0.5) * 2 * lifeVariance,
+					velocity: velocity.toArray(),
+					scale: scale + (rng() - 0.5) * 2 * scaleVariance,
+					position: attachToEmitter ? [0, 0, 0] : emitterPosition.toArray(),
 				};
 				mesh.userData.instanceProps.set(i, instanceProps);
 			}
-			instanceProps.lifetime = instanceProps.lifetime + delta;
+			instanceProps.age = instanceProps.age + delta;
+			const instanceLifeRatio = instanceProps.age / instanceProps.lifespan;
+			// Scale the particle over its lifetime
+			const scalePercent =
+				scalePercentOverLife?.getPoint(instanceLifeRatio) ?? 1;
+			meshScale.setScalar(instanceProps.scale * scalePercent);
 
-			position.addScaledVector(
-				forward.set(0, 0, 1),
-				instanceProps.speed * delta,
-			);
+			// Adjust the color
+			if (colorOverLife) {
+				mesh.setColorAt(i, colorOverLife.getPoint(instanceLifeRatio));
+			}
 
-			instanceMatrix.compose(position, quaternion, scale);
+			position
+				.set(...instanceProps.position)
+				.addScaledVector(forward.set(...instanceProps.velocity), delta);
+			instanceProps.position = position.toArray();
+			if (!attachToEmitter) {
+				position.sub(emitterPosition);
+			}
+
+			instanceMatrix.compose(position, quaternion, meshScale);
 			mesh.setMatrixAt(i, instanceMatrix);
 
 			// Deactivate the particle if it's time
-			if (instanceProps.lifetime > instanceProps.lifespan) {
+			if (instanceProps.age > instanceProps.lifespan) {
 				const activeParticleIndex = mesh.userData.activeParticles.indexOf(i);
 				mesh.userData.activeParticles.splice(activeParticleIndex, 1);
 				mesh.userData.inactiveParticles.push(i);
 				mesh.userData.instanceProps.delete(i);
 			}
 		}
-
+		if (colorOverLife && mesh.instanceColor) {
+			mesh.instanceColor.needsUpdate = true;
+		}
 		mesh.instanceMatrix.needsUpdate = true;
 	});
 	return (
@@ -249,21 +372,40 @@ function calculateMaxParticles(
 			initialCount,
 	);
 }
+
 // https://github.com/simondevyoutube/ThreeJS_Tutorial_ParticleSystems/blob/master/main.js
 // MIT Copyright (c) 2020 simondevyoutube
+const lerpVector = new Vector3();
+const lerpColor = new Color();
+function defaultLerp<T>(a: T, b: T, t: number) {
+	if (typeof a === "number" && typeof b === "number")
+		return (a + t * (b - a)) as T;
+	if (a instanceof Vector3 && b instanceof Vector3)
+		return lerpVector.lerpVectors(a, b, t) as T;
+	if (a instanceof Color && b instanceof Color)
+		return lerpColor.lerpColors(a, b, t) as T;
+
+	throw new Error("No lerp function provided");
+}
 class LinearSpline<T> {
 	private points: [number, T][];
-	private lerp: (t: number, a: T, b: T) => T;
-	constructor(lerp: (t: number, a: T, b: T) => T) {
-		this.points = [];
-		this.lerp = lerp;
+	private lerp: (a: T, b: T, t: number) => T;
+	constructor({
+		points,
+		lerp,
+	}: { points?: [number, T][]; lerp?: (a: T, b: T, t: number) => T }) {
+		this.points = points || [];
+		this.lerp = lerp!;
+		if (!this.lerp) {
+			this.lerp = defaultLerp;
+		}
 	}
 
-	AddPoint(t: number, d: T) {
+	addPoint(t: number, d: T) {
 		this.points.push([t, d]);
 	}
 
-	Get(t: number) {
+	getPoint(t: number) {
 		let p1 = 0;
 
 		for (let i = 0; i < this.points.length; i++) {
@@ -280,9 +422,25 @@ class LinearSpline<T> {
 		}
 
 		return this.lerp(
-			(t - this.points[p1][0]) / (this.points[p2][0] - this.points[p1][0]),
 			this.points[p1][1],
 			this.points[p2][1],
+			(t - this.points[p1][0]) / (this.points[p2][0] - this.points[p1][0]),
 		);
 	}
+}
+
+export function mergeRefs<T = any>(
+	refs: Array<
+		React.MutableRefObject<T> | React.LegacyRef<T> | undefined | null
+	>,
+): React.RefCallback<T> {
+	return (value) => {
+		refs.forEach((ref) => {
+			if (typeof ref === "function") {
+				ref(value);
+			} else if (ref != null) {
+				(ref as React.MutableRefObject<T | null>).current = value;
+			}
+		});
+	};
 }
