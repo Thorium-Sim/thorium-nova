c2a48a4c9004667c36d80ec86cb8f1bf7d17f8a8
Alex Anderson <hey@ralexanderson.com>
2024-05-21 21:09:10 -0400

    Refactor into useParticles

--- client/app/routes/3d/route.tsx
+++ client/app/routes/3d/route.tsx
@@ -1,5 +1,12 @@
-import { OrbitControls, Stars } from "@react-three/drei";
-import { Canvas, type GroupProps, useFrame } from "@react-three/fiber";
+import { OrbitControls, Stars, Stats } from "@react-three/drei";
+import {
+	Canvas,
+	type GroupProps,
+	useFrame,
+	useLoader,
+	type SpriteMaterialProps,
+	useThree,
+} from "@react-three/fiber";
 import {
 	type ReactNode,
 	useMemo,
@@ -16,9 +23,261 @@ import {
 	Vector3,
 	Quaternion,
 	Color,
-	Group,
+	type Group,
 	MathUtils,
+	AdditiveBlending,
+	Object3D,
+	TextureLoader,
+	DoubleSide,
+	Sprite,
 } from "three";
+import blob from "./blob.png?url";
+
+const instanceMatrix = new Matrix4();
+const meshSpin = new Quaternion();
+const velocity = new Vector3();
+const forward = new Vector3(0, 0, 1);
+const up = new Vector3(0, 1, 0);
+
+interface CellProps {
+	birthRatePerSecond?: number;
+	birthRateVariance?: number;
+	initialCount?: number;
+	lifeInSeconds?: number;
+	lifeVariance?: number;
+	speed?: number;
+	speedVariance?: number;
+	colorOverLife?: LinearSpline<Color>;
+	scale?: number;
+	scaleVariance?: number;
+	scalePercentOverLife?: LinearSpline<number>;
+	angle?: Partial<Angle>;
+	angleVariance?: Partial<Angle>;
+	spin?: Partial<Spin>;
+	spinVariance?: Partial<Spin>;
+	attachToEmitter?: boolean;
+	// TODO
+	alignAngleWithVelocity?: boolean;
+
+	rng?: () => number;
+}
+function useParticles({
+	birthRatePerSecond = 1,
+	birthRateVariance = 0,
+	initialCount = 0,
+	lifeInSeconds = 1,
+	lifeVariance = 0,
+	speed = 1,
+	speedVariance = 0,
+	colorOverLife,
+	scale = 1,
+	scaleVariance = 0,
+	scalePercentOverLife,
+	angle = {},
+	angleVariance = {},
+	spin = {},
+	spinVariance = {},
+	attachToEmitter = false,
+	rng = Math.random,
+}: CellProps) {
+	const { camera } = useThree();
+	const userDataRef = useRef<{
+		activeParticles: number[];
+		inactiveParticles: number[];
+		timeToNextParticle: number;
+		instanceProps: Map<number, InstanceProps>;
+	} | null>(null);
+	const { emissionAngleRange, getEmitterPosition } = useEmitter();
+
+	const maxParticles = useMemo(
+		() =>
+			calculateMaxParticles(
+				birthRatePerSecond,
+				birthRateVariance,
+				initialCount,
+				lifeInSeconds,
+				lifeVariance,
+			),
+		[
+			birthRatePerSecond,
+			initialCount,
+			lifeVariance,
+			birthRateVariance,
+			lifeInSeconds,
+		],
+	);
+
+	useFrame((state, delta) => {
+		if (!userDataRef.current) {
+			// @ts-ignore
+			userDataRef.current = {};
+		}
+		const userData = userDataRef.current;
+		if (!userData) throw new Error("userData is null");
+		if (!userData.instanceProps) {
+			userData.instanceProps = new Map();
+			userData.activeParticles = [];
+			userData.inactiveParticles = [];
+			userData.timeToNextParticle =
+				1 / (birthRatePerSecond + (rng() - 0.5) * 2 * birthRateVariance);
+			for (let i = 0; i < initialCount; i++) {
+				userData.activeParticles.push(i);
+			}
+			for (let i = initialCount; i < maxParticles; i++) {
+				userData.inactiveParticles.push(i);
+			}
+		}
+
+		userData.timeToNextParticle -= delta;
+		while (userData.timeToNextParticle <= 0) {
+			const difference = Math.abs(userData.timeToNextParticle);
+			if (userData.inactiveParticles.length > 0) {
+				const particleIndex = userData.inactiveParticles.pop();
+				userData.activeParticles.push(particleIndex!);
+			}
+			const rate = birthRatePerSecond + (rng() - 0.5) * 2 * birthRateVariance;
+			userData.timeToNextParticle = 1 / rate;
+			// We might need to emit two or more particles in a single frame. This makes sure that happens.
+			userData.timeToNextParticle -= difference;
+		}
+	});
+
+	function updateParticle(
+		index: number,
+		transform: Object3D,
+		color: Color,
+		delta: number,
+	) {
+		const userData = userDataRef.current;
+		if (!userData) throw new Error("userData is null");
+
+		const emitterPosition = getEmitterPosition();
+
+		// If the particle is dead, skip it
+		if (!userData.activeParticles.includes(index)) {
+			transform.scale.setScalar(0);
+			return transform;
+		}
+		up.set(0, 1, 0);
+
+		let instanceProps: InstanceProps | undefined =
+			userData.instanceProps.get(index);
+
+		// Initialize the particle
+		if (!instanceProps) {
+			transform.position.set(0, 0, 0);
+			transform.quaternion.set(0, 0, 0, 1);
+			transform.scale.set(1, 1, 1);
+			color.set(1, 1, 1);
+			const speedValue = speed + (rng() - 0.5) * 2 * speedVariance;
+
+			// Randomize the direction of the particle
+			{
+				const phi =
+					(rng() - 0.5) * 2 * MathUtils.degToRad(emissionAngleRange.longitude);
+				const theta =
+					(rng() - 0.5) * 2 * MathUtils.degToRad(emissionAngleRange.latitude);
+				const x = Math.cos(theta) * Math.sin(phi);
+				const y = Math.sin(theta);
+				const z = Math.cos(theta) * Math.cos(phi);
+				velocity.set(x, y, z).normalize().multiplyScalar(speedValue);
+			}
+
+			// Randomize the rotation of the particle
+			{
+				const theta =
+					MathUtils.degToRad(angle.latitude || 0) +
+					(rng() - 0.5) * 2 * MathUtils.degToRad(angleVariance.latitude || 0);
+				const phi =
+					MathUtils.degToRad(angle.longitude || 0) +
+					(rng() - 0.5) * 2 * MathUtils.degToRad(angleVariance.longitude || 0);
+				const x = Math.cos(theta) * Math.cos(phi);
+				const y = Math.cos(theta) * Math.sin(phi);
+				const z = Math.sin(theta);
+				// Reusing this vector to avoid creating a new one
+				forward.set(x, y, z);
+				transform.quaternion.setFromUnitVectors(up, forward);
+			}
+
+			// Randomize the spin of the particle
+			const spinVal: [number, number, number] = [
+				(rng() - 0.5) * 2 * (spinVariance.x || 0) + (spin.x || 0),
+				(rng() - 0.5) * 2 * (spinVariance.y || 0) + (spin.x || 0),
+				(rng() - 0.5) * 2 * (spinVariance.z || 0) + (spin.x || 0),
+			];
+
+			instanceProps = {
+				age: 0,
+				lifespan: lifeInSeconds + (rng() - 0.5) * 2 * lifeVariance,
+				velocity: velocity.toArray(),
+				scale: scale + (rng() - 0.5) * 2 * scaleVariance,
+				position: attachToEmitter ? [0, 0, 0] : emitterPosition.toArray(),
+				spin: spinVal,
+			};
+			userData.instanceProps.set(index, instanceProps);
+		}
+
+		// Update the particle
+		instanceProps.age = instanceProps.age + delta;
+		const instanceLifeRatio = instanceProps.age / instanceProps.lifespan;
+		// Scale the particle over its lifetime
+		const scalePercent = scalePercentOverLife?.getPoint(instanceLifeRatio) ?? 1;
+		transform.scale.setScalar(instanceProps.scale * scalePercent);
+
+		// Adjust the color
+		if (colorOverLife) {
+			color.set(colorOverLife.getPoint(instanceLifeRatio));
+		}
+
+		transform.position
+			.set(...instanceProps.position)
+			.addScaledVector(forward.set(...instanceProps.velocity), delta);
+		instanceProps.position = transform.position.toArray();
+		if (!attachToEmitter) {
+			transform.position.sub(emitterPosition);
+		}
+
+		// Spin the particle
+		forward.set(...instanceProps.spin);
+		const magnitude = forward.length();
+		if (magnitude > 0) {
+			// Half angle
+			const thetaOverTwo = (magnitude * delta) / 2.0;
+			const sinThetaOverTwo = Math.sin(thetaOverTwo);
+
+			// Normalize omega
+			const axis = forward.normalize();
+
+			// Construct the quaternion
+			meshSpin
+				.set(
+					axis.x * sinThetaOverTwo,
+					axis.y * sinThetaOverTwo,
+					axis.z * sinThetaOverTwo,
+					Math.cos(thetaOverTwo),
+				)
+				.normalize();
+			transform.quaternion.multiply(meshSpin);
+		}
+		transform.rotation.y = Math.atan2(
+			camera.position.x - transform.position.x,
+			camera.position.z - transform.position.z,
+		);
+
+		transform.updateMatrix();
+
+		// Deactivate the particle if it's time
+		if (instanceProps.age > instanceProps.lifespan) {
+			const activeParticleIndex = userData.activeParticles.indexOf(index);
+			userData.activeParticles.splice(activeParticleIndex, 1);
+			userData.inactiveParticles.push(index);
+			userData.instanceProps.delete(index);
+		}
+		return transform;
+	}
+
+	return { maxParticles, updateParticle };
+}
 
 export default function ThreeD() {
 	return (
@@ -40,57 +299,98 @@ function Test() {
 		if (!ref.current) return;
 		ref.current.position.y = Math.sin(state.clock.elapsedTime) * 2;
 	});
+	const texture = useLoader(TextureLoader, blob);
 	return (
-		<Emitter
-			ref={ref}
-			rotation={[0, Math.PI / 2, 0]}
-			emissionAngleRange={{ latitude: 0, longitude: 0 }}
-		>
-			<mesh>
-				<boxGeometry attach="geometry" args={[0.5, 0.5, 0.5]} />
-				<meshBasicMaterial wireframe attach="material" color="white" />
-			</mesh>
-			<Cell
-				initialCount={0}
-				lifeInSeconds={1}
-				lifeVariance={0.5}
-				birthRatePerSecond={20}
-				birthRateVariance={10}
-				speed={2}
-				speedVariance={0.9}
-				attachToEmitter
-				// angleVariance={{ longitude: 180, latitude: 90 }}
-				spin={{ longitude: 0, latitude: 1 }}
-				colorOverLife={
-					new LinearSpline({
-						points: [
-							[0, new Color("yellow")],
-							[0.9, new Color("red")],
-						],
-					})
-				}
-				scaleVariance={0.5}
-				scalePercentOverLife={
-					new LinearSpline({
-						points: [
-							[0, 0],
-							[0.2, 1],
-							[0.9, 1],
-							[1, 0],
-						],
-					})
-				}
+		<>
+			<Emitter
+				ref={ref}
+				position={[-3, 0, 0]}
+				rotation={[0, Math.PI / 2, 0]}
+				emissionAngleRange={{ latitude: 2, longitude: 2 }}
 			>
-				<coneGeometry attach="geometry" args={[0.5, 0.75, 3, 1]} />
-				<meshBasicMaterial wireframe attach="material" color="white" />
-			</Cell>
-		</Emitter>
+				<SpriteCell
+					map={texture}
+					blending={AdditiveBlending}
+					initialCount={0}
+					lifeInSeconds={3}
+					lifeVariance={0.5}
+					birthRatePerSecond={20}
+					birthRateVariance={10}
+					speed={2}
+					speedVariance={0.9}
+					colorOverLife={
+						new LinearSpline({
+							points: [
+								[0, new Color("yellow")],
+								[0.9, new Color("red")],
+							],
+						})
+					}
+					scaleVariance={0.5}
+					scalePercentOverLife={
+						new LinearSpline({
+							points: [
+								[0, 0],
+								[0.2, 1],
+								[0.9, 1],
+								[1, 0],
+							],
+						})
+					}
+				/>
+				{/* <MeshCell
+					initialCount={0}
+					lifeInSeconds={3}
+					lifeVariance={0.5}
+					birthRatePerSecond={20}
+					birthRateVariance={10}
+					speed={2}
+					speedVariance={0.9}
+					colorOverLife={
+						new LinearSpline({
+							points: [
+								[0, new Color("yellow")],
+								[0.9, new Color("red")],
+							],
+						})
+					}
+					scaleVariance={0.5}
+					scalePercentOverLife={
+						new LinearSpline({
+							points: [
+								[0, 0],
+								[0.2, 1],
+								[0.9, 1],
+								[1, 0],
+							],
+						})
+					}
+				>
+					<icosahedronGeometry attach="geometry" args={[0.5, 0]} />
+					<meshBasicMaterial
+						attach="material"
+						color="white"
+						wireframe
+						blending={AdditiveBlending}
+						depthTest={true}
+						depthWrite={false}
+						side={DoubleSide}
+						transparent
+					/>
+				</MeshCell> */}
+			</Emitter>
+		</>
 	);
 }
 
 interface Angle {
 	longitude: number;
 	latitude: number;
+}
+interface Spin {
+	x: number;
+	y: number;
+	z: number;
 }
 const EmitterContext = createContext<{
 	emissionAngleRange: Angle;
@@ -144,14 +444,6 @@ const Emitter = forwardRef<
 
 Emitter.displayName = "Emitter";
 
-const instanceMatrix = new Matrix4();
-const position = new Vector3();
-const quaternion = new Quaternion();
-const meshScale = new Vector3();
-const meshColor = new Color();
-const velocity = new Vector3();
-const forward = new Vector3(0, 0, 1);
-const up = new Vector3(0, 1, 0);
 interface InstanceProps {
 	/** How long the particle has been alive */
 	age: number;
@@ -161,11 +453,55 @@ interface InstanceProps {
 	velocity: [number, number, number];
 	/** How big the particle is */
 	scale: number;
-	/** The position of the emitter when the particle was created */
+	/** The position particle. We need to keep this separate because of attachToEmitter*/
 	position: [number, number, number];
+	/** The angular velocity (spin) of the particle */
+	spin: [number, number, number];
 }
-function Cell({
+const transform = new Object3D();
+const color = new Color();
+function MeshCell({
 	children,
+	...cellProps
+}: {
+	children: ReactNode;
+} & CellProps) {
+	const ref = useRef<InstancedMesh>(null);
+
+	const { maxParticles, updateParticle } = useParticles(cellProps);
+	useFrame((state, delta) => {
+		if (!ref.current) return;
+
+		const { current: mesh } = ref;
+
+		for (let i = 0; i < maxParticles; i++) {
+			mesh.getMatrixAt(i, instanceMatrix);
+			instanceMatrix.decompose(
+				transform.position,
+				transform.quaternion,
+				transform.scale,
+			);
+			updateParticle(i, transform, color, delta);
+			mesh.setColorAt(i, color);
+			mesh.setMatrixAt(i, transform.matrix);
+		}
+		if (mesh.instanceColor) {
+			mesh.instanceColor.needsUpdate = true;
+		}
+		mesh.instanceMatrix.needsUpdate = true;
+	});
+	return (
+		<instancedMesh
+			ref={ref}
+			args={[undefined, undefined, maxParticles]}
+			frustumCulled={false}
+		>
+			{children}
+		</instancedMesh>
+	);
+}
+
+function SpriteCell({
 	birthRatePerSecond = 1,
 	birthRateVariance = 0,
 	initialCount = 0,
@@ -179,184 +515,53 @@ function Cell({
 	scalePercentOverLife,
 	angle = {},
 	angleVariance = {},
+	spin = {},
+	spinVariance = {},
 	attachToEmitter = false,
 	rng = Math.random,
-}: {
-	children: ReactNode;
-	birthRatePerSecond?: number;
-	birthRateVariance?: number;
-	initialCount?: number;
-	lifeInSeconds?: number;
-	lifeVariance?: number;
-	speed?: number;
-	speedVariance?: number;
-	colorOverLife?: LinearSpline<Color>;
-	scale?: number;
-	scaleVariance?: number;
-	scalePercentOverLife?: LinearSpline<number>;
-	angle?: Partial<Angle>;
-	angleVariance?: Partial<Angle>;
-	spin?: Partial<Angle>;
-	spinVariance?: Partial<Angle>;
-
-	attachToEmitter?: boolean;
-	// TODO
-	alignAngleWithVelocity?: boolean;
-
-	rng?: () => number;
-}) {
-	const ref = useRef<InstancedMesh>(null);
-	const { emissionAngleRange, getEmitterPosition } = useEmitter();
-
-	const maxParticles = useMemo(
-		() =>
-			calculateMaxParticles(
-				birthRatePerSecond,
-				birthRateVariance,
-				initialCount,
-				lifeInSeconds,
-				lifeVariance,
-			),
-		[
-			birthRatePerSecond,
-			initialCount,
-			lifeVariance,
-			birthRateVariance,
-			lifeInSeconds,
-		],
-	);
-
+	...spriteMaterialProps
+}: CellProps & SpriteMaterialProps) {
+	const { maxParticles, updateParticle } = useParticles({
+		birthRatePerSecond,
+		birthRateVariance,
+		initialCount,
+		lifeInSeconds,
+		lifeVariance,
+		speed,
+		speedVariance,
+		colorOverLife,
+		scale,
+		scaleVariance,
+		scalePercentOverLife,
+		angle,
+		angleVariance,
+		spin,
+		spinVariance,
+		attachToEmitter,
+		rng,
+	});
+	const ref = useRef<Group>(null);
 	useFrame((state, delta) => {
 		if (!ref.current) return;
-		const emitterPosition = getEmitterPosition();
-
-		const { current: mesh } = ref;
-		if (!mesh.userData.instanceProps) {
-			mesh.userData.instanceProps = new Map();
-			mesh.userData.activeParticles = [];
-			mesh.userData.inactiveParticles = [];
-			mesh.userData.timeToNextParticle =
-				1 / (birthRatePerSecond + (rng() - 0.5) * 2 * birthRateVariance);
-			for (let i = 0; i < initialCount; i++) {
-				mesh.userData.activeParticles.push(i);
-			}
-			for (let i = initialCount; i < maxParticles; i++) {
-				mesh.userData.inactiveParticles.push(i);
-			}
-		}
-
-		mesh.userData.timeToNextParticle -= delta;
-		while (mesh.userData.timeToNextParticle <= 0) {
-			const difference = Math.abs(mesh.userData.timeToNextParticle);
-			if (mesh.userData.inactiveParticles.length > 0) {
-				const particleIndex = mesh.userData.inactiveParticles.pop();
-				mesh.userData.activeParticles.push(particleIndex);
-			}
-			const rate = birthRatePerSecond + (rng() - 0.5) * 2 * birthRateVariance;
-			mesh.userData.timeToNextParticle = 1 / rate;
-			// We might need to emit two or more particles in a single frame. This makes sure that happens.
-			mesh.userData.timeToNextParticle -= difference;
-		}
 
 		for (let i = 0; i < maxParticles; i++) {
-			mesh.getMatrixAt(i, instanceMatrix);
-			instanceMatrix.decompose(position, quaternion, meshScale);
-
-			// If the particle is dead, skip it
-			if (!mesh.userData.activeParticles.includes(i)) {
-				meshScale.setScalar(0);
-				instanceMatrix.compose(position, quaternion, meshScale);
-				mesh.setMatrixAt(i, instanceMatrix);
-				continue;
-			}
-
-			let instanceProps: InstanceProps = mesh.userData.instanceProps.get(i);
-
-			// Initialize the particle
-			if (!instanceProps) {
-				position.set(0, 0, 0);
-				quaternion.set(0, 0, 0, 1);
-				meshScale.set(1, 1, 1);
-				meshColor.set(1, 1, 1);
-				const speedValue = speed + (rng() - 0.5) * 2 * speedVariance;
-
-				// Randomize the direction of the particle
-				{
-					const phi =
-						(rng() - 0.5) *
-						2 *
-						MathUtils.degToRad(emissionAngleRange.longitude);
-					const theta =
-						(rng() - 0.5) * 2 * MathUtils.degToRad(emissionAngleRange.latitude);
-					const x = Math.cos(theta) * Math.sin(phi);
-					const y = Math.sin(theta);
-					const z = Math.cos(theta) * Math.cos(phi);
-					velocity.set(x, y, z).normalize().multiplyScalar(speedValue);
-				}
-				// Randomize the rotation of the particle
-				{
-					const latitude =
-						(angle.latitude || 0) +
-						(rng() - 0.5) * 2 * (angleVariance.latitude || 0);
-					const longitude =
-						(angle.longitude || 0) +
-						(rng() - 0.5) * 2 * (angleVariance.longitude || 0);
-					const x = Math.cos(latitude) * Math.cos(longitude);
-					const y = Math.cos(latitude) * Math.sin(longitude);
-					const z = Math.sin(latitude);
-					// Reusing this vector to avoid creating a new one
-					forward.set(x, y, z);
-					quaternion.setFromUnitVectors(up, forward);
-				}
-				instanceProps = {
-					age: 0,
-					lifespan: lifeInSeconds + (rng() - 0.5) * 2 * lifeVariance,
-					velocity: velocity.toArray(),
-					scale: scale + (rng() - 0.5) * 2 * scaleVariance,
-					position: attachToEmitter ? [0, 0, 0] : emitterPosition.toArray(),
-				};
-				mesh.userData.instanceProps.set(i, instanceProps);
-			}
-			instanceProps.age = instanceProps.age + delta;
-			const instanceLifeRatio = instanceProps.age / instanceProps.lifespan;
-			// Scale the particle over its lifetime
-			const scalePercent =
-				scalePercentOverLife?.getPoint(instanceLifeRatio) ?? 1;
-			meshScale.setScalar(instanceProps.scale * scalePercent);
-
-			// Adjust the color
-			if (colorOverLife) {
-				mesh.setColorAt(i, colorOverLife.getPoint(instanceLifeRatio));
-			}
-
-			position
-				.set(...instanceProps.position)
-				.addScaledVector(forward.set(...instanceProps.velocity), delta);
-			instanceProps.position = position.toArray();
-			if (!attachToEmitter) {
-				position.sub(emitterPosition);
-			}
-
-			instanceMatrix.compose(position, quaternion, meshScale);
-			mesh.setMatrixAt(i, instanceMatrix);
-
-			// Deactivate the particle if it's time
-			if (instanceProps.age > instanceProps.lifespan) {
-				const activeParticleIndex = mesh.userData.activeParticles.indexOf(i);
-				mesh.userData.activeParticles.splice(activeParticleIndex, 1);
-				mesh.userData.inactiveParticles.push(i);
-				mesh.userData.instanceProps.delete(i);
+			const child = ref.current.children[i];
+			if (!child) continue;
+			if ("material" in child) {
+				// @ts-ignore
+				updateParticle(i, child, child.material.color, delta);
 			}
 		}
-		if (colorOverLife && mesh.instanceColor) {
-			mesh.instanceColor.needsUpdate = true;
-		}
-		mesh.instanceMatrix.needsUpdate = true;
 	});
+
 	return (
-		<instancedMesh ref={ref} args={[undefined, undefined, maxParticles]}>
-			{children}
-		</instancedMesh>
+		<group ref={ref}>
+			{Array.from({ length: maxParticles }).map((_, i) => (
+				<sprite key={i}>
+					<spriteMaterial attach="material" {...spriteMaterialProps} />
+				</sprite>
+			))}
+		</group>
 	);
 }
 
