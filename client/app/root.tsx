d728b94ded31e1c6d1c91234fc7082c5dbd1d4b4
Alex Anderson <hey@ralexanderson.com>
2024-07-24 20:05:40 -0400

    fix: Disable scaling the browser viewport on mobile devices.

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -69,7 +69,10 @@ function Document({ children }: { children: ReactNode }) {
 		<html lang="en">
 			<head>
 				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+				/>
 				<link rel="icon" type="image/svg+xml" href={icon} />
 				<Meta />
 				<Links />
