1f8c8078b761b7269efa65c30bbc51217e299229
Alex Anderson <hey@ralexanderson.com>
2025-01-25 19:53:21 -0700

    Tauri App

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,6 @@
 import { thoriumPath } from "@thorium/utils/.server/appPaths";
 import { zip } from "@thorium/utils/.server/zip";
-import { execSync } from "node:child_process";
+import { exec, type ExecException } from "node:child_process";
 import { mkdir, rm } from "node:fs/promises";
 import path from "node:path";
 
@@ -34,12 +34,48 @@ for await (const filename of glob.scan({
 writer.end();
 
 console.info("Bundling server");
+const platformMap = {
+	"aarch64-apple-darwin": "bun-darwin-arm64",
+	// Bun doesn't support arm on Windows yet, so we'll hope emulation works
+	"aarch64-pc-window-msvc": "bun-windows-x64",
+	"aarch64-unknown-linux-gnu": "bun-linux-arm64",
+	"x86_64-pc-windows-msvc": "bun-windows-x64",
+	"x86_64-unknown-linux-gnu": "bun-linux-x64",
+	"x86_64-apple-darwin": "bun-darwin-x64",
+};
+
+const targetArch =
+	process.arch === "arm64"
+		? "aarch64"
+		: process.arch === "x64"
+			? "x86_64"
+			: "unknown";
+const targetPlatform =
+	process.platform === "darwin"
+		? "apple-darwin"
+		: process.platform === "linux"
+			? "unknown-linux-gnu"
+			: process.platform === "win32"
+				? "pc-windows-msvc"
+				: "unknown";
+
+const arch = (process.env.BUILD_ARCH ||
+	`${targetArch}-${targetPlatform}`) as keyof typeof platformMap;
+
 // We have to run this next command using a shell since the --compile flag doesn't work with the Bun API.
-execSync(
-	`bun build --minify --define 'process.env.NODE_ENV="production"' --sourcemap --outfile build/bunServer --compile ./app/server.ts`,
+const command = `bun build --minify --define 'process.env.NODE_ENV="production"' --target=TARGET --sourcemap --outfile ./src-tauri/binaries/thoriumNovaServer-ARCH --compile ./app/server.ts`;
+const target = platformMap[arch];
+if (!target) {
+	throw new Error(
+		`Invalid arch or platform. Arch: ${process.arch}, Platform: ${process.platform}.`,
+	);
+}
+await new Promise<void>((res, rej) =>
+	exec(
+		command.replace("TARGET", target).replace("ARCH", arch),
+		(err: ExecException | null) => (err ? rej(err) : res()),
+	),
 );
 
 console.info("Cleaning up");
-await rm(path.join("./build/defaultPlugin.plug"));
-await rm(path.join("./build/client"), { force: true, recursive: true });
-await rm(path.join("./build/clientBundle.dat"));
+await rm(path.join("./build"), { force: true, recursive: true });
