6f7c0b554da3de9df41886844159e388c4afaac4
Alex Anderson <hey@ralexanderson.com>
2025-05-09 21:27:32 -0600

    fix: Address an issue with clicking the yaw or forward/reverse thruster controls.

--- app/hooks/useJoystick.ts
+++ app/hooks/useJoystick.ts
@@ -37,42 +37,40 @@ export function useJoystick({
 		offsetRef.current = xy;
 		set({ xy, immediate: true });
 	}
-	const bind = useDrag(
-		({ down, first, movement: [x, y] }) => {
-			if (first) {
-				maxDistance.current =
-					(containerRef.current?.getBoundingClientRect()[
-						axis === "y" ? "height" : "width"
-					] || 0) / 2;
-			}
-			dragDown.current = down;
-			x += offsetRef.current[0];
-			y += offsetRef.current[1];
-			const dist = distance(x, y);
-			if (dist > maxDistance.current) {
-				const theta = Math.abs(Math.atan(y / x));
-				x = maxDistance.current * Math.cos(theta) * (x > 0 ? 1 : -1);
-				y = maxDistance.current * Math.sin(theta) * (y > 0 ? 1 : -1);
-			}
-			if (
-				axisSnap &&
-				(Math.abs(x) > minDistance || Math.abs(y) > minDistance)
-			) {
-				if (x < minDistance && x > minDistance * -1) x = 0;
-				if (y < minDistance && y > minDistance * -1) y = 0;
-			}
-			set({
-				xy: down ? [x, y] : [0, 0],
-				immediate: down,
-			});
-			callback.current?.(
-				down
-					? { x: x / maxDistance.current, y: y / maxDistance.current }
-					: { x: 0, y: 0 },
-			);
-		},
-		{ axis },
-	);
+	const bind = useDrag(({ down, first, movement: [x, y] }) => {
+		if (first) {
+			maxDistance.current =
+				(containerRef.current?.getBoundingClientRect()[
+					axis === "y" ? "height" : "width"
+				] || 0) / 2;
+		}
+		dragDown.current = down;
+		x += offsetRef.current[0];
+		y += offsetRef.current[1];
+		const dist = distance(x, y);
+		if (dist > maxDistance.current) {
+			const theta = Math.abs(Math.atan(y / x));
+			x = maxDistance.current * Math.cos(theta) * (x > 0 ? 1 : -1);
+			y = maxDistance.current * Math.sin(theta) * (y > 0 ? 1 : -1);
+		}
+		if (axisSnap && (Math.abs(x) > minDistance || Math.abs(y) > minDistance)) {
+			if (x < minDistance && x > minDistance * -1) x = 0;
+			if (y < minDistance && y > minDistance * -1) y = 0;
+		}
+		set({
+			xy: down
+				? axis
+					? [axis === "x" ? x : 0, axis === "y" ? y : 0]
+					: [x, y]
+				: [0, 0],
+			immediate: down,
+		});
+		callback.current?.(
+			down
+				? { x: x / maxDistance.current, y: y / maxDistance.current }
+				: { x: 0, y: 0 },
+		);
+	});
 
 	const gamepadValues = useRef([0, 0]);
 
