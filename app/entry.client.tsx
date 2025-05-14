91f5936ec528a3b148395ec28161457995fb8a19
Alex Anderson <hey@ralexanderson.com>
2025-05-14 14:25:45 -0600

    Cleanup

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,18 +1,21 @@
 import { HydratedRouter } from "react-router/dom";
-import { startTransition, StrictMode } from "react";
+import { startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
+
+export interface ThoriumGlobal {
+	// Kiosk helpers
+	clipboardWriteText: (text: string) => Promise<boolean>;
+	getAddress: () => Promise<string>;
+	getHostSecret: () => Promise<string>;
+	registerUpdateHandler: (handler: (update: string) => void) => void;
+}
 
 // TODO Aug 23 2021 - Configure this with the methods that are available
 // via Tauri IPC
 declare global {
 	interface Window {
 		isHeadless: boolean;
-		thorium: {
-			clipboardWriteText: (text: string) => Promise<boolean>;
-			getAddress: () => Promise<string>;
-			getHostSecret: () => Promise<string>;
-			registerUpdateHandler: (handler: (update: string) => void) => void;
-		};
+		thorium: ThoriumGlobal;
 	}
 }
 
