501e94fe3dd0d5724b4bb339e4f0083c49870a46
Alex Anderson <hey@ralexanderson.com>
2025-08-29 18:53:18 -0600

    Damage report editor start

--- app/routes/debug.tsx
+++ app/routes/debug.tsx
@@ -1,9 +1,51 @@
-import { LightsOut } from "@thorium/components/MiniGames/LightsOut";
-import Button from "@thorium/ui/Button";
-import { cn } from "@thorium/utils/cn";
-import { produce } from "immer";
-import { useState } from "react";
+import { Editor } from "@monaco-editor/react";
 
 export default function () {
-	return <LightsOut onComplete={() => alert("You win!")} />;
+	return (
+		<Editor
+			defaultValue={`# Title: Remote Access Code
+
+# damageTypes: Electrical, Structural
+
+// Variables provided to every report
+VAR damageType = "Electrical"
+VAR damageMetric = "instability"
+VAR secondaryMetric = "cascadeRisk"
+VAR system = "Warp Engines"
+VAR playerShip = 1
+EXTERNAL uppercase(input)
+
+// Local variables just used for this report
+VAR plural = false
+VAR code = ""
+
+The <>{damageType:
+- "Electrical": 
+~ plural = true
+electrical subsystems
+- else: structure
+}<> of the {system} {plural:have|has} sustained damage, causing <>{damageMetric:
+- "efficiency": a loss in efficiency
+- "instability": commands sent to the system to fail.
+}
+
+~ code = "{~Alpha|Beta|Gamma|Delta|Zeta|Lambda|Omicron}-{RANDOM(100,999)}-{uppercase(system)}-{~Ansible|Cyber|Matrix|Naidon|Skadov|Memory|Faraday|Bernal|Dyson|Protocol|Vector|Analog|Digital|Buffer|Cache|Crypto|Fragment|System|Duplex|Threading|Hyper|Interlace|Progressive|Simplex|Multiplex|Syntax|Token}"
+
+Send the following remote access code to initiate repairs: \`{code}\`
+
+* [remoteAccess.send? entity: {playerShip} code: {code}]
+
+The report has been completed successfully.
+
+-> END
+
+=== function uppercase(input)
+~ return "WARPENGINES"`}
+			theme="vs-dark"
+			options={{
+				minimap: { enabled: false },
+				wordWrap: "on",
+			}}
+		/>
+	);
 }
